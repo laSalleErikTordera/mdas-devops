805886938e939bd53ac5f82e04f6803eb582a0c9
Erik Tordera Bermejo <erik.tordera@students.salle.edu.url>
2019-10-29 20:22:13 +0100

    added pipeline.sh

---
+++ bin/pipeline.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# CI/CD Pipeline for the project votingapp
+
+# Dependencies Install
+go get github.com/gorilla/websocket
+go get github.com/labstack/echo
+
+# Go Build
+go build -o build/votingapp/votingapp ./src/votingapp
+
+# Copy static files
+mkdir build
+mkdir build/votingapp
+cp -r src/votingapp/ui build/votingapp/ui
