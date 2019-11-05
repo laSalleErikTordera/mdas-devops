8fb77926c8402f05650d975d61f5f91304cffc21
Erik Tordera Bermejo <erik.tordera@students.salle.edu.url>
2019-11-05 19:15:09 +0100

    Modified pipeline.py

---
+++ bin/pipeline.py
@@ -0,0 +1,57 @@
+def setUp():
+
+    print("Starting Pipeline Setup....")
+    print("Finished Pipeline Setup....")
+
+
+def dependencies():
+
+    print("Starting dependencies install....")
+    print("Finished dependencies install....")
+
+
+def build():
+
+    print("Starting project building....")
+    print("Finished project building....")
+
+
+def run():
+
+    print("Starting program execution....")
+    print("Finished program execution....")
+
+
+def testSetUp():
+
+    print("Starting testing setUp....")
+    print("Finished testing setUp....")
+
+
+def test():
+
+    print("Starting testing....")
+    print("Finished testing....")
+
+
+def testCleanUp():
+
+    print("Starting test clean up....")
+    print("Finished test clean up....")
+
+
+def cleanUp():
+
+    print("Starting pipeline clean up....")
+    print("Finished pipeline clean up....")
+
+
+setUp()
+dependencies()
+build()
+run()
+testSetUp()
+test()
+testCleanUp()
+cleanUp()
+
