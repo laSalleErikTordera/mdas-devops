43f700623403b387ca9c185822b09970ca9c1ebb
Erik Tordera Bermejo <erik.tordera@students.salle.edu.url>
2019-11-13 19:11:20 +0100

    Finished Deployment

--- bin/pipelineK8s.sh
+++ bin/pipelineK8s.sh
@@ -4,8 +4,15 @@ set -e
 docker-compose rm -f || true
 docker-compose up --build -d && \
 docker-compose run --rm mytest && \
-#docker-compose push && \
+docker-compose push && \
 echo "GREEN" || echo "RED"
 
 #deploy
 kubectl apply -f votingapp.yaml
+
+
+kubectl run mytest \
+	--generator=run-pod/v1 \
+	--image=lasalleeriktordera/votingapp-test \
+	--env VOTINGAPP_HOST=myvotingapp.votingapp \
+	--rm --attach --restart=Never
